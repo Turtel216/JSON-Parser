6c08385cfa20b0b2dc0a9cfca321b62649d7e988
Turtel216 <papadimitrios216@gmail.com>
2024-11-04 18:02:26 +0200

    Added AST type

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,12 @@
 module Main where
 
+data JsonValue = JsonNull
+                | JsonBool Bool 
+                | JsonNumber Integer
+                | JsonString String
+                | JsonArray [JsonValue]
+                | JsonObject [(String, JsonValue)]
+                deriving (Show, Eq)
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
