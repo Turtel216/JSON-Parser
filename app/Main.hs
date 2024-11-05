8ebfaa132622265f9b08d5f1f7b70807790e270e
Turtel216 <papadimitrios216@gmail.com>
2024-11-05 17:00:17 +0200

    Implemented Parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,8 @@
 module Main where
 
+import Control.Applicative
+import Data.Char
+
 data JsonValue = JsonNull
                 | JsonBool Bool 
                 | JsonNumber Integer -- Implement Floating point numbers
@@ -26,8 +29,13 @@ instance Applicative Parser where
       (input'', a) <- p2 input'
       Just (input'', f a)
 
+instance Alternative Parser where 
+  empty = Parser $ \_ -> Nothing
+  (Parser p1) <|> (Parser p2) =
+    Parser $ \input -> p1 input <|> p2 input
+
 jsonNull :: Parser JsonValue 
-jsonNull = undefined
+jsonNull = (\_ -> JsonNull) <$> stringP "null"
 
 
 -- parses single character
@@ -43,8 +51,61 @@ charP x = Parser f
 stringP :: String -> Parser String
 stringP input = sequenceA $ map charP input
 
+jsonBool :: Parser JsonValue
+jsonBool = f <$> (stringP "true" <|> stringP "false")
+  where f "true"  = JsonBool True
+        f "false" = JsonBool False
+        -- This should never happen
+        f _       = undefined
+
+-- A version of span from stdlib but for the parser type
+spanP :: (Char -> Bool) -> Parser String
+spanP f = 
+  Parser $ \input -> 
+    let (token, rest) = span f input
+      in Just (rest, token)
+
+notNull :: Parser [a] -> Parser [a]
+notNull (Parser p) = 
+  Parser $ \input -> do
+    (input', xs) <- p input 
+    if null xs 
+      then Nothing
+      else Just (input', xs)
+
+-- Seperates input by giving seperator
+sepBy :: Parser a -> Parser b -> Parser [b]
+sepBy sep element = (:) <$> element <*> many (sep *> element) <|> pure []
+
+whiteSpace :: Parser String
+whiteSpace = spanP isSpace
+
+-- TODO add escape support
+stringLiteral :: Parser String 
+stringLiteral = charP '"' *> spanP (/= '"') <* charP '"'
+
+jsonString :: Parser JsonValue 
+jsonString = JsonString <$> stringLiteral 
+
+jsonNumber :: Parser JsonValue 
+jsonNumber = f <$> notNull (spanP isDigit)
+  where f ds = JsonNumber $ read ds
+
+jsonArray :: Parser JsonValue 
+jsonArray = JsonArray <$> (charP '[' *> whiteSpace *> elements <* whiteSpace <* charP ']')
+  where elements = sepBy sep jsonValue
+        sep = whiteSpace *> charP ',' <* whiteSpace
+
+jsonObject :: Parser JsonValue 
+jsonObject = JsonObject <$> (charP '{'
+    *> whiteSpace *>
+    sepBy (whiteSpace *> charP ',' <* whiteSpace) pair
+    <* whiteSpace <*
+    charP '}')
+  where pair = (\key _ value-> (key, value)) <$> stringLiteral <*> (whiteSpace *> charP ':' *> whiteSpace) <*> jsonValue
+
 jsonValue :: Parser JsonValue 
-jsonValue = undefined
+jsonValue = jsonNull <|> jsonBool <|> jsonNumber <|> jsonString <|> jsonArray <|> jsonObject
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = undefined
